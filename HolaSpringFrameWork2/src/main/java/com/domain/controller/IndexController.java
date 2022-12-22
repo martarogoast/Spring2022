7c8b65ac23285c7d815da7611f503fc5bee47e15
Rodríguez González <marta.rodriguez-gonzalez@capgemini.com>
2022-12-22 08:14:13 +0100

    Spring core

---
+++ HolaSpringFrameWork2/src/main/java/com/domain/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.domain.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping("/home")
+	public String goIndex() {
+		return "Index";
+	}
+}
